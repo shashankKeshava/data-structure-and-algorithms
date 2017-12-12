9a80e252a0a0219c20a2166738f26e564e12fa6d
Shashank <shashank@BigAdmins-Mac-minis.local>
2017-12-12 17:27:27 +0530

    Yield Example

---
+++ jobCrunch/yieldExample.js
@@ -0,0 +1,20 @@
+function* countAppleSales () {
+    var saleList = [3, 7, 5];
+    a = 0
+    for (var i = 0; i < saleList.length - 1; i++) {
+        a=yield saleList[i];
+        console.log(a);
+    }
+    return console.log(a);
+  }
+  var appleStore = countAppleSales(); // Generator { }
+  console.log(appleStore.next(a)); // { value: 3, done: false }
+  debugger
+
+  function a() {
+     return 1
+  }
+
+  console.log(appleStore.next()); // { value: 7, done: false }
+  console.log(appleStore.next()); // { value: 5, done: false }
+  console.log(appleStore.next(a)); // { value: undefined, done: true }
